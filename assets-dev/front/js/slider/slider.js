99cd1ea9ce32bc734c59de6190da7ea816f77aa6
Przemysław Dziadek <przemyslaw.dziadek@gmail.com>
2017-05-16 18:43:41 +0200

    new style and js mechnism

--- assets-dev/front/js/slider/slider.js
+++ assets-dev/front/js/slider/slider.js
@@ -1,3 +1,40 @@
 jQuery( document ).ready( function( $ ) {
-    $('.wizzaro-slider').carousel({ keyboard: false });
+    $('.wizzaro-slider').each(function() {
+        var slider = $(this);
+        var interval = slider.attr('data-interval'),
+            pauseOnHover = slider.attr('data-pause-on-hover'),
+            useArrows = slider.attr('data-use-arrows'),
+            useBullets = slider.attr('data-use-bullets');
+
+        if ( ! interval ) {
+            interval = 4000;
+        }
+
+        if ( pauseOnHover == '1' ) {
+            pauseOnHover = true;
+        } else {
+            pauseOnHover = false;
+        }
+
+        if ( useArrows == '1' ) {
+            useArrows = true;
+        } else {
+            useArrows = false;
+        }
+
+        if ( useBullets == '1' ) {
+            useBullets = true;
+        } else {
+            useBullets = false;
+        }
+
+
+        slider.slick({
+            autoplay: false,//true,
+            arrows: useArrows,
+            dots: useBullets,
+            pauseOnHover: pauseOnHover,
+            autoplaySpeed: interval,
+        });
+    });
 });
--- view/wizzaro/slider/controller/shortcode/shordcode.php
+++ view/wizzaro/slider/controller/shortcode/shordcode.php
@@ -2,55 +2,28 @@
 $totalSlides = count( $view_data['slides'] );
 if ( $totalSlides > 0 ) {
     $slider_id = 'wizzaro-slider-' . $view_data['unique_id'];
+    $use_arrows = ( ! isset( $view_data['attrs']['use_arrows'] ) || $view_data['attrs']['use_arrows'] == '1' );
+    $use_bullets = ( ! isset( $view_data['attrs']['use_bullets'] ) || $view_data['attrs']['use_bullets'] == '1' );
     ?>
-    <div id="<?php echo $slider_id; ?>" class="wizzaro-slider slide" data-ride="wizzaro-slider" data-interval="<?php echo esc_attr( $view_data['settings']['interval'] ); ?>" data-pause="<?php echo $view_data['settings']['pause_on_hover'] == 1 ? 'hover' : 'false' ?>">
+    <div id="<?php echo $slider_id; ?>" class="wizzaro-slider slide" role="listbox" data-ride="wizzaro-slider" data-interval="<?php echo esc_attr( $view_data['settings']['interval'] ); ?>" data-pause-on-hover="<?php echo $view_data['settings']['pause_on_hover'] == 1 ? 'hover' : 'false' ?>" data-use-arrows="<?php echo $use_arrows ? '1' : '0'; ?>" data-use-bullets="<?php echo $use_bullets ? '1' : '0'; ?>">
         <?php
-        if ( ! isset( $view_data['attrs']['use_bullets'] ) || $view_data['attrs']['use_bullets'] == '1' ) {
+        $first = true;
+        foreach ( $view_data['slides'] as $slide ) {
             ?>
-            <ol class="ws-indicators">
+            <div class="ws-item<?php echo $first ? ' first': ''; ?>">
+                <img src="<?php echo wp_get_attachment_url( $slide['image_id'], 'wizzaro-slider-image-size' ); ?>" alt="" />
                 <?php
-                for ( $i = 0; $i < $totalSlides; $i++ ) {
+                if ( mb_strlen( $slide['content'] ) > 0 ) {
                     ?>
-                    <li data-target="#<?php echo $slider_id; ?>" data-slide-to="<?php echo $i; ?>"<?php echo $i === 0 ? ' class="active"' : ''; ?>></li>
+                    <div class="ws-caption">
+                        <?php echo $slide['content']; ?>
+                    </div>
                     <?php
                 }
                 ?>
-            </ol>
+            </div>
             <?php
-        }
-        ?>
-        <div class="ws-inner" role="listbox">
-            <?php
-            $first = true;
-            foreach ( $view_data['slides'] as $slide ) {
-                ?>
-                <div class="ws-item<?php echo $first ? ' active': ''; ?>">
-                    <img src="<?php echo wp_get_attachment_url( $slide['image_id'], 'wizzaro-slider-image-size' ); ?>" alt="" />
-                    <?php
-                    if ( mb_strlen( $slide['content'] ) > 0 ) {
-                        ?>
-                        <div class="ws-caption">
-                            <?php echo $slide['content']; ?>
-                        </div>
-                        <?php
-                    }
-                    ?>
-                </div>
-                <?php
-                $first = false;
-            }
-            ?>
-        </div>
-        <?php
-        if ( ! isset( $view_data['attrs']['use_arrows'] ) || $view_data['attrs']['use_arrows'] == '1' ) {
-            ?>
-            <a class="ws-control-prev" href="#<?php echo $slider_id; ?>" role="button" data-slide="prev">
-                <?php _e( 'Prev', $view_data['languages_domain'] ); ?>
-            </a>
-            <a class="ws-control-next" href="#<?php echo $slider_id; ?>" role="button" data-slide="next">
-                <?php _e( 'Next', $view_data['languages_domain'] ); ?>
-            </a>
-            <?php
+            $first = false;
         }
         ?>
     </div>
